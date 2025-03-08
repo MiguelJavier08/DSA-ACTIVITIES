c4147bb173f5d0ddd8f8ee0fb33ac2572da0dca4
MiguelJavier08 <haviermigol@gmail.com>
2025-03-08 12:36:13 +0900

    Activity 2 part 1

---
+++ Javier_Miguel_lab_2.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct Node{
+    string songName;
+    Node *link;
+}Node;
+
+Node *createNode(string data){
+    Node *newNode = new Node;
+    newNode->songName = data;
+    newNode->link = NULL;
+
+    return newNode;
+}
+
+void traverse(Node *head){
+    Node *temp = new Node;
+    temp = head;
+
+    cout << "My Playlist" <<endl;
+    while(temp != NULL){
+        cout << temp->songName<< "->"<<endl;
+        if(temp->link == NULL){
+            cout << "NULL"<<endl;
+        }
+        
+        temp = temp->link;
+    }
+}        
+
+Node *insertAtEnd(string data, Node *head){
+    if(head == NULL){
+        Node *newNode = createNode(data);
+        head = newNode;
+        cout << "A new node has been inserted at the end \n" << endl;
+        return head;
+    }
+    Node *temp = new Node;
+    temp = head;
+    
+    while(temp->link != NULL){
+        temp = temp->link;
+    }
+    
+    Node *newNode = createNode(data);
+    temp->link = newNode;
+
+    cout << "A new node has been inserted at the end \n" << endl;
+    return head;
+}    
+
+Node *insertAtBeginning(string data, Node *head){
+    Node *newNode = createNode(data);
+    newNode->link = head;
+
+    head = newNode;
+
+    cout << "A new node has been inserted at the beginning \n" << endl;
+    return head;
+}
+
+string insertAfter(string after, string data, Node *head){
+    Node *temp = new Node;
+    temp = head;
+
+    while(temp->songName.compare(after) !=0){
+        if(temp == NULL){
+            return "No such song exist, please try again later.";
+        }
+        
+        temp = temp->link;
+    }
+    Node *newNode = createNode(data);
+    newNode->link = temp->link;
+    temp->link = newNode;
+    
+    return "An new node has been added after " + after + "\n";
+}   
+
+int main(){
+    Node *head = createNode("Sanctuary by Miguel Pogi");
+    head = insertAtEnd("Sunday Morning by Miguel Pogi", head);
+    insertAtEnd("Cherry On Top by BINI", head);
+    insertAtEnd("Blink Twice by BINI", head);
+    
+    head = insertAtBeginning("Paninindigan kita by Ben&Ben", head);
+    head = insertAtBeginning("TAKE ALL THE LOVE by Tj Monterde", head);
+    head = insertAtBeginning("Marilag by Dionela", head);
+    head = insertAtBeginning("Gitara by Parokya Ni Edgar", head);
+
+
+    
+    
+    
+    traverse(head);
+ 
+
+
+    return 0;
+}    
